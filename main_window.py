d527d24f39812f2eb46cde04279c3c94064d44c8
Carlos <carlosdanielrt@gmail.com>
2024-04-14 12:27:07 -0400

    [ADD] implement the operations buttons

--- main_window.py
+++ main_window.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLineEdit, QPushButton, QLabel
 from matrix import Matrix
+from matrix_calculator import MatrixOperations
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -28,9 +29,31 @@ class MainWindow(QMainWindow):
         self.result_label = QLabel()
         layout.addWidget(self.result_label)
 
+        self.addition_button = QPushButton("Sumar Matrices")
+        layout.addWidget(self.addition_button)
+
+        self.substraction_button = QPushButton("Restar Matrices")
+        layout.addWidget(self.substraction_button)
+
+        self.multiplication_button = QPushButton("Multiplicar Matrices")
+        layout.addWidget(self.multiplication_button)
+
+        self.division_button = QPushButton("Dividir Matrices")
+        layout.addWidget(self.division_button)
+
+        self.operations_result_label_title = QLabel("Resultado:")
+        layout.addWidget(self.operations_result_label_title)
+        self.operations_result_label = QLabel()
+        layout.addWidget(self.operations_result_label)
+
         central_widget.setLayout(layout)
 
         self.generate_button.clicked.connect(self.generate_matrices)
+
+        self.addition_button.clicked.connect(self.action_add_matrix)
+        self.substraction_button.clicked.connect(self.action_substract_matrix)
+        self.multiplication_button.clicked.connect(self.action_multiply_matrix)
+        self.division_button.clicked.connect(self.action_divide_matrix)
 
     def generate_matrices(self):
         rows = int(self.rows_input.text())
@@ -38,4 +61,20 @@ class MainWindow(QMainWindow):
         self.matrix_a = Matrix(rows, cols)
         self.matrix_b = Matrix(rows, cols)
         self.result_label.setText(str(self.matrix_a) + "\n\n" + str(self.matrix_b))
+
+    def action_add_matrix(self):
+        self.matrix_result = MatrixOperations.add(self.matrix_a, self.matrix_b)
+        self.operations_result_label.setText(str(self.matri))
+
+    def action_substract_matrix(self):
+        self.matrix_result = MatrixOperations.subtract(self.matrix_a, self.matrix_b)
+        self.operations_result_label.setText(str(self.matri))
+
+    def action_multiply_matrix(self):
+        self.matrix_result = MatrixOperations.multiply(self.matrix_a, self.matrix_b)
+        self.operations_result_label.setText(str(self.matri))
+
+    def action_divide_matrix(self):
+        self.matrix_result = MatrixOperations.divide(self.matrix_a, self.matrix_b)
+        self.operations_result_label.setText(str(self.matri))
 
